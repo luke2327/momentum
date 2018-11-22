b39939c4236256547c654fd2407ab2c968162872
luke2327 <qwepoi8526@gmail.com>
2018-11-22 16:51:43 +0900

    Bug Fix

--- todo.js
+++ todo.js
@@ -59,9 +59,9 @@ function loadToDos() {
     const loadedToDos = localStorage.getItem(TODOS_LS);
     if (loadedToDos !== null) {
         const parsedToDos = JSON.parse(loadedToDos);
-        // parsedToDos.forEach(function (toDo) {
-        //     paintToDo(toDo.text);
-        // })
+        parsedToDos.forEach(function (toDo) {
+            paintToDo(toDo.text);
+        })
     }
 }
 
